c88f039bde3d3919401a5025e47ca8c6cf36e33d
xbosshub-byte <x.bosshub@gmail.com>
2026-03-01 21:27:36 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ado',
+    version='1.0.0',
+    description='A lightweight IoT protocol (Broker & Client) by Boss',
+    author='Boss',
+    py_modules=['ado'], # ระบุชื่อไฟล์ ado.py ที่ต้องการทำเป็นไลบรารี
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: POSIX :: Linux',
+    ],
+)
